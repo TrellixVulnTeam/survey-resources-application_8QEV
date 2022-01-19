2bd3afde441cc1c24bc85167d1435cc435e579f6
[dbouchard10] <dbouchard@co.morris.nj.us>
2022-01-19 15:26:21 -0500

    Atempting to match Steve's settings in his working project

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,10 +6,8 @@ import { HomeComponent } from './home.component';
 import { Shell } from '@app/shell/shell.service';
 
 const routes: Routes = [
-  Shell.childRoutes([
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent, data: { title: marker('Home') } },
-  ]),
 ];
 
 @NgModule({
