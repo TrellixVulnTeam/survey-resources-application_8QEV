d20ace9536a761297db043e5991de5ad271e0b08
[dbouchard10] <dbouchard@co.morris.nj.us>
2022-01-20 14:20:48 -0500

    Remove map from home to fix bugged application

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,48 +7,10 @@ import { SharedModule } from '@shared';
 import { MaterialModule } from '@app/material.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { EsriMapModule, ESRI_MAP_CONFIG } from 'esri-map';
-
-const mapConfig = {
-  mapServiceUrl:
-    'https://morrisgisapps.co.morris.nj.us/arcgis105/rest/services/ParcelSearcher/ParcelSearcher_Dynamic/MapServer',
-  nearmapKey: '',
-  baseMaps: ['topo-vector', 'hybrid'],
-  sublayerConfig: [
-    {
-      id: 4,
-      title: 'Recent Sales',
-      visible: true,
-      legendEnabled: true,
-      listMode: 'show',
-    },
-    {
-      id: 7,
-      title: 'Parcels',
-      visible: true,
-      legendEnabled: true,
-      listMode: 'show',
-    },
-
-    {
-      id: 14,
-      title: 'Municipalities',
-      visible: true,
-      legendEnabled: false,
-      listMode: 'hide',
-    },
-  ],
-};
-
 
 @NgModule({
-  imports: [CommonModule, TranslateModule, SharedModule, FlexLayoutModule, MaterialModule, HomeRoutingModule, EsriMapModule],
+  imports: [CommonModule, TranslateModule, SharedModule, FlexLayoutModule, MaterialModule, HomeRoutingModule],
   declarations: [HomeComponent],
-  providers: [
-    {
-      provide: ESRI_MAP_CONFIG,
-      useValue: mapConfig,
-    },
-  ],
+  providers: [  ],
 })
 export class HomeModule {}
