7efd4dd8e98b0613ca3876356f3d298cfa6f60d2
[dbouchard10] <dbouchard@co.morris.nj.us>
2022-02-03 15:32:19 -0500

    Data results to model Steve's Tax-Board app

--- src/app/@shared/data-results/data-results.component.ts
+++ src/app/@shared/data-results/data-results.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { DataCardComponent } from '../data-card/data-card.component';
 
+import { DataPagingComponent } from '../data-paging/data-paging.component';
+import { SurveyResourcesCard } from '@app/@core/data/models/isurveyresources';
+
+import { IState, State } from '@app/@core/data/models/state';
+import { SurveyResourcesServiceService } from '@app/@core/data/survey-resources-service.service';
 @Component({
   selector: 'app-data-results',
   templateUrl: './data-results.component.html',
-  styleUrls: ['./data-results.component.scss'],
+  styleUrls: ['../data-container/data-container.component.scss', './data-results.component.scss'],
 })
 export class DataResultsComponent implements OnInit {
-  constructor() {}
+  surveyResources$: SurveyResourcesCard[] = [];
+  constructor(private surveyResourcesService: SurveyResourcesServiceService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.surveyResourcesService.items$.subscribe((b) => {
+      this.surveyResources$ = b;
+    });
+  }
 }
